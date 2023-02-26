cc53aacf3dac12a5860e09668847d7647fd3b449
hotequil <hotequil@protonmail.com>
2023-02-26 14:17:53 -0300

    feat: removed env for debug

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from prefixes import root_prefix
 from dotenv import load_dotenv, find_dotenv
-from os import environ
 
 load_dotenv(find_dotenv())
 
@@ -13,4 +12,4 @@ CORS(app, resources={f"/{root_prefix}/*": { "origins": "*" }})
 import projects.accounts.routes.users_routes
 
 if __name__ == '__main__':
-    app.run(debug=eval(environ.get("DEBUG")), port=8000)
+    app.run(debug=True, port=8000)
