8f9c3b1f9c4c0cb5e15732531e16a6f8fd008a10
hotequil <hotequil@protonmail.com>
2023-02-26 14:08:20 -0300

    feat: done app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from prefixes import root_prefix
+from dotenv import load_dotenv, find_dotenv
+from os import environ
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+CORS(app, resources={f"/{root_prefix}/*": { "origins": "*" }})
+
+import projects.accounts.routes.users_routes
+
+if __name__ == '__main__':
+    app.run(debug=eval(environ.get("DEBUG")), port=8000)
